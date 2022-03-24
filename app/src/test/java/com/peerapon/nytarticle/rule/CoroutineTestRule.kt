610787ba9daf2cd282b60f56682816513ac0da64
Peerapon C <peerapon01@gmail.com>
2022-03-25 03:15:25 +0700

    revert CoroutineTestRule.kt

--- app/src/test/java/com/peerapon/nytarticle/rule/CoroutineTestRule.kt
+++ app/src/test/java/com/peerapon/nytarticle/rule/CoroutineTestRule.kt
@@ -10,21 +10,21 @@ import org.junit.runners.model.Statement
 @ExperimentalCoroutinesApi
 class CoroutineTestRule : TestRule {
 
-    private val testCoroutineDispatcher = StandardTestDispatcher()
-    private val testCoroutineScope = TestScope(testCoroutineDispatcher)
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
 
     override fun apply(base: Statement, description: Description?) = object : Statement() {
         @Throws(Throwable::class)
         override fun evaluate() {
             Dispatchers.setMain(testCoroutineDispatcher)
-
             base.evaluate()
-
             Dispatchers.resetMain()
+            testCoroutineScope.cleanupTestCoroutines()
         }
     }
 
-    fun runBlockingTest(block: suspend TestScope.() -> Unit) =
-        testCoroutineScope.runTest { block() }
+    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
+        testCoroutineScope.runBlockingTest { block() }
 
 }
